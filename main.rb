5cca46f87d0481ec61e32f529ec7db160cf455cc
chu <chuusatetsuya@chuusatetsuyanoMacBook-Pro.local>
2021-02-08 21:04:25 +0900

    再度インテンド、変数修正

--- main.rb
+++ main.rb
@@ -1,12 +1,13 @@
+#encoding: utf-8
 require "./methods.rb"
 
 plans = [
   { place: "沖縄", price: 10000 },
   { place: "北海道", price: 20000 },
-  { place: "九州", price: 15000 }
+  { place: "九州", price: 15000 },
 ]
 
 plans_menu(plans)
 selected_plan = select_plan(plans)
-decide_count_people = decide_how_many_people(selected_plan)
-fee_calculation(selected_plan,decide_count_people)
+how_many_people = decide_how_many_people(selected_plan)
+fee_calculation(selected_plan, how_many_people)
--- methods.rb
+++ methods.rb
@@ -1,40 +1,39 @@
-
+#encoding: utf-8
 def plans_menu(plans)
   puts "旅行プランを選択してください。"
-  plans.each.with_index(1) do |plan,i|
-   puts "#{i}.#{plan[:place]}旅行(#{plan[:price]}円)"
+  plans.each.with_index(1) do |plan, i|
+    puts "#{i}.#{plan[:place]}旅行(#{plan[:price]}円)"
   end
 end
 
 def select_plan(plans)
   while true
-  print "プランの番号を選択 >"
-  select_plan_num = gets.to_i
-  break if (1..3).include?(select_plan_num)
-  puts "1~3の番号を選択してください。"
+    print "プランの番号を選択 >"
+    select_plan_num = gets.to_i
+    break if (1..3).include?(select_plan_num)
+    puts "1~3の番号を選択してください。"
   end
-  plans[select_plan_num - 1]    
+  plans[select_plan_num - 1]
 end
 
-def decide_how_many_people(selected_plan)   
+def decide_how_many_people(selected_plan)
   puts "#{selected_plan[:place]}旅行ですね。"
   while true
-   puts "何名で予約されますか？"
-   print "人数を入力 >"
-   how_many_people = gets.to_i
-   break if how_many_people > 0 && how_many_people.is_a?(Integer)
-   puts "1人以上を入力してください。"
-end
+    puts "何名で予約されますか？"
+    print "人数を入力 >"
+    how_many_people = gets.to_i
+    break if how_many_people > 0 && how_many_people.is_a?(Integer)
+    puts "1人以上を入力してください。"
+  end
   puts "#{how_many_people}名ですね。"
   how_many_people
 end
 
-def fee_calculation(selected_plan,decide_count_people)
-  total_price = selected_plan[:price] * decide_count_people
-  if decide_count_people >= 5
-  puts "5名以上ですので10%割引となります。"
-  total_price *= 0.9
-end
+def fee_calculation(selected_plan, how_many_people)
+  total_price = selected_plan[:price] * how_many_people
+  if how_many_people >= 5
+    puts "5名以上ですので10%割引となります。"
+    total_price *= 0.9
+  end
   puts "合計金額は#{total_price.floor}円になります。"
 end
-
